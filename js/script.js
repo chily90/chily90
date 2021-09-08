a1d4ea87243742ed926207d4c759c0220defd3aa
chily90 <90252054+chily90@users.noreply.github.com>
2021-09-08 17:41:53 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-chily97","displayName":"chily97","link":{"linkType":"LinkTypePage","href":"#!page-chily97"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
